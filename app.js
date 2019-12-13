8b2a0363b95b5da8cbedfb1958d32f74f51c7634
Anders <kiss7medical@gmail.com>
2019-12-12 22:49:00 -0500

    setup JSON response for random users

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-var url = 'https://randomuser.me/api/';
+// CHANGE TO 20 RESULTS
+// var url = 'https://randomuser.me/api/?results=2';
+var url = 'https://randomuser.me/api/?nat=us,ca&results=2';
 
 app.get('/', function(req, res) {
 	request(url, function(error, response, body) {
 		var random_data = JSON.parse(body);
 		console.log(random_data);
+
+		var random_user = {
+			gender: random_data.gender,
+			// first_name: random_data.name.first,
+			// last_name: random_data.name.last,
+			country: random_data.nat,
+			// date_of_birth: random_data.dob.date,
+			birthday: ' ',
+		};
+
+		console.log('>> This is a single user: ' + random_data.name);
 
 		res.render('random');
 	});
