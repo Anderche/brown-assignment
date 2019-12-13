f5c43d80f3cdc741e02f57158558983ca5341a10
Anders <kiss7medical@gmail.com>
2019-12-12 20:29:07 -0500

    add request package && add get request to randomuser.me API

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const request = require('request');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
+var url = 'https://randomuser.me/api/';
+
 app.get('/', function(req, res) {
-	res.render('random');
+	request(url, function(error, response, body) {
+		var random_data = JSON.parse(body);
+		console.log(random_data);
+
+		res.render('random');
+	});
 });
 
 app.listen(3000, () => {
