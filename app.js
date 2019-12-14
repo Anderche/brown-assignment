016a8e9f83e05b924893d9412cd43799f1fb3a99
Anders <kiss7medical@gmail.com>
2019-12-13 22:14:40 -0500

    get Birthday conditional functional

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var dateFormat = require('dateformat');
+
 const express = require('express');
 const request = require('request');
 
@@ -6,10 +8,16 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // CHANGE TO 20 RESULTS
-// var url = 'https://randomuser.me/api/?results=2';
+// var url = 'https://randomuser.me/api/?nat=us,ca&results=20';
 var url = 'https://randomuser.me/api/?nat=us,ca&results=2';
 
 var passUsers = [];
+// var currentDate = Date();
+
+var currentDate = new Date().toISOString().split('T', 1);
+var monthDay = dateFormat(currentDate, 'mm-dd');
+console.log('currently it is: ' + monthDay);
+console.log('---- 1 ----');
 
 app.get('/', function(req, res) {
 	request(url, function(error, response, body) {
@@ -24,22 +32,32 @@ app.get('/', function(req, res) {
 				first_name: person.name.first,
 				last_name: person.name.last,
 				country: person.nat,
-				date_of_birth: person.dob.date,
+				date_of_birth: person.dob.date.split('T', 1),
 				birthday: ' ',
 			};
 
+			// push random user object into passUsers array
 			passUsers.push(random_user);
 
-			console.log('-----------------');
-			console.log(random_user);
+			console.log('---- 2 ----');
+			var dob = person.dob.date.split('T', 1);
+			console.log(dob);
+			var formatted_dob = dateFormat(dob, 'UTC:mm-dd');
+			console.log('User D.O.B. is: ' + formatted_dob);
+
+			// push birthday into passUsers
+			// passUsers.push(bdayText);
+
+			console.log('---- 3 ----');
+
+			if (formatted_dob < monthDay) console.log('Birthday has happened already!');
+			else if (formatted_dob > monthDay) console.log('Birthday has yet to occur!');
+			else console.log('Birthday is today!');
 		}
 
-		console.log('>> This is the number of random users: ' + random_data.length);
 		console.log('------');
 		console.log(passUsers);
-
 		console.log('------');
-		console.log(passUsers[0].first_name);
 
 		res.render('random');
 	});
